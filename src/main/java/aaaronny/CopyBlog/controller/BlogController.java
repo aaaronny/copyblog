0753b2416779db45afd051d204492ece7e92db21
AAARonny <35843460+aaaronny@users.noreply.github.com>
2019-04-08 12:46:54 +0200

    Update BlogController.java

--- src/main/java/aaaronny/CopyBlog/controller/BlogController.java
+++ src/main/java/aaaronny/CopyBlog/controller/BlogController.java
@@ -1,7 +1,6 @@
 package aaaronny.CopyBlog.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,5 +25,23 @@ public class BlogController {
 
 		return response;
 	}
+
+	@RequestMapping(value = "/image", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
+	public ResponseEntity<String> getImg(@RequestParam("url") String url) {
+
+		ResponseEntity<String> response = null;
+
+		RestTemplate restTemplate = new RestTemplate();
+		String res = restTemplate.getForObject(url, String.class);
+		if (!res.equals("null")) {
+			response = new ResponseEntity<String>(res, HttpStatus.OK);
+		} else {
+			response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+		return response;
+	}
+	
+	//produces = MediaType.IMAGE_JPEG_VALUE
 
 }
