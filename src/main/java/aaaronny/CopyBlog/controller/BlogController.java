6f3921e15b6d707e063a8cd0060f2a4dde8006cd
AAARonny <35843460+aaaronny@users.noreply.github.com>
2019-04-08 13:08:52 +0200

    Update BlogController.java

--- src/main/java/aaaronny/CopyBlog/controller/BlogController.java
+++ src/main/java/aaaronny/CopyBlog/controller/BlogController.java
@@ -1,6 +1,9 @@
 package aaaronny.CopyBlog.controller;
 
 import org.springframework.http.*;
+import java.io.*;
+import java.net.*;
+import java.utils.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,38 +13,50 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class BlogController {
 
-	@RequestMapping(value = "/copia", method = RequestMethod.GET)
-	public ResponseEntity<String> getOldMsg(@RequestParam("url") String url) {
+    @RequestMapping(value = "/copia", method = RequestMethod.GET)
+    public ResponseEntity<String> getOldMsg(@RequestParam("url") String url) {
 
-		ResponseEntity<String> response = null;
+        ResponseEntity<String> response = null;
 
-		RestTemplate restTemplate = new RestTemplate();
-		String res = restTemplate.getForObject(url, String.class);
-		if (!res.equals("null")) {
-			response = new ResponseEntity<String>(res, HttpStatus.OK);
-		} else {
-			response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
+        RestTemplate restTemplate = new RestTemplate();
+        String res = restTemplate.getForObject(url, String.class);
+        if (!res.equals("null")) {
+            response = new ResponseEntity<String>(res, HttpStatus.OK);
+        } else {
+            response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
-		return response;
-	}
+        return response;
+    }
 
-	@RequestMapping(value = "/image", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
-	public ResponseEntity<String> getImg(@RequestParam("url") String url) {
+    @RequestMapping(value = "/image", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getImg(@RequestParam("url") String path) {
 
-		ResponseEntity<String> response = null;
+        ResponseEntity<String> response = null;
 
-		RestTemplate restTemplate = new RestTemplate();
-		String res = restTemplate.getForObject(url, String.class);
-		if (!res.equals("null")) {
-			response = new ResponseEntity<String>(res, HttpStatus.OK);
-		} else {
-			response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
+        URL url = new URL(path);
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        URLConnection conn = url.openConnection();
+        conn.setRequestProperty("User-Agent", "Firefox");
 
-		return response;
-	}
-	
-	//produces = MediaType.IMAGE_JPEG_VALUE
+        try (InputStream inputStream = conn.getInputStream()) {
+            int n = 0;
+            byte[] buffer = new byte[1024];
+            while (-1 != (n = inputStream.read(buffer))) {
+                output.write(buffer, 0, n);
+            }
+        }
+        byte[] img = output.toByteArray();
+        
+        if (!res.equals("null")) {
+            response = new ResponseEntity<byte[]>(img, HttpStatus.OK);
+        } else {
+            response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return response;
+    }
+
+    //produces = MediaType.IMAGE_JPEG_VALUE
 
 }
