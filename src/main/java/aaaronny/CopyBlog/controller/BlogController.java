ec749ae8ee00a364491f25b2cf7cb7baa4bdf4af
AAARonny <35843460+aaaronny@users.noreply.github.com>
2021-03-04 09:33:30 +0100

    Update BlogController.java

--- src/main/java/aaaronny/CopyBlog/controller/BlogController.java
+++ src/main/java/aaaronny/CopyBlog/controller/BlogController.java
@@ -88,6 +88,19 @@ public class BlogController {
         return response;
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @RequestMapping(value = "/sptest", method = RequestMethod.GET)
+    public String sharepoint() {
+
+        String response = null;
+        String url = "https://kmakers.sharepoint.com:443/_api/v2.0/drives/b!ZDi6MsRb-Uy_NyBaY1DFadVE-GemSj1LpS89F9v-vxm-pyxK9HlIRL19Vahd11jl/items/01IE2KTKK563XHAQ7YAJHJ4LW3VN4BARRF?version=Published&access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAva21ha2Vycy5zaGFyZXBvaW50LmNvbUAwMTQwOTY0OC1jNTUxLTQ4YTEtOWQ4MC01ZTNmODJlYmYyYTEiLCJpc3MiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAiLCJuYmYiOiIxNjE0ODM3NjAwIiwiZXhwIjoiMTYxNDg1OTIwMCIsImVuZHBvaW50dXJsIjoiNDY0b1lJak5wbytlRDJGZllBSU9GSkZTNER5V08wSjdtaitCNWlvQ1VnRT0iLCJlbmRwb2ludHVybExlbmd0aCI6IjExNCIsImlzbG9vcGJhY2siOiJUcnVlIiwidmVyIjoiaGFzaGVkcHJvb2Z0b2tlbiIsInNpdGVpZCI6Ik16SmlZVE00TmpRdE5XSmpOQzAwWTJZNUxXSm1NemN0TWpBMVlUWXpOVEJqTlRZNSIsInNpZ25pbl9zdGF0ZSI6IltcImttc2lcIl0iLCJuYW1laWQiOiIwIy5mfG1lbWJlcnNoaXB8YWxlc3NhbmRyby5hcGljZWxsYUBzdGVwc3JsLm9yZyIsIm5paSI6Im1pY3Jvc29mdC5zaGFyZXBvaW50IiwiaXN1c2VyIjoidHJ1ZSIsImNhY2hla2V5IjoiMGguZnxtZW1iZXJzaGlwfDEwMDM3ZmZlYTQ1NTdlNDBAbGl2ZS5jb20iLCJ0dCI6IjAiLCJ1c2VQZXJzaXN0ZW50Q29va2llIjoiMyJ9.bW5mellHblNBaVBhSzhaakNmOSsrMk5Ja0F3Q1o1dHJPcG9BYnNHdGF4QT0";
+
+        RestTemplate restTemplate = new RestTemplate();
+        String res = restTemplate.getForObject(url, String.class);
+            
+        return res;
+    }
+
     //produces = MediaType.IMAGE_JPEG_VALUE
 
 }
